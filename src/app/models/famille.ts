52b59321a781b13bcb121f2d9f693ab4fd28d797
Bastien Deshayes <bastien.deshayes@kelkoogroup.com>
2019-04-06 19:53:45 +0200

    Configuration de l'application Angular. Ajout des modèles pour les articles, fournisseurs et familles.

---
+++ src/app/models/famille.ts
@@ -0,0 +1,21 @@
+export interface Article {
+  code: number;
+  designation: string;
+  code_tva: number;
+  qte_appro: number;
+  prix_vente: number;
+  anc_prix_vente: number;
+  prix_promo: number;
+  prix_achat_brut: number;
+  anc_prix_achat_brut: number;
+  remise_achat: number;
+  status: string;
+  qte_kg_litre: number;
+  unite_vente: string;
+  date_creation: Date;
+  date_modification: Date;
+  taux_tva: number;
+  prix_vente_ht: number;
+  anc_prix_vente_ht: number;
+
+}
