96b979837589fd8c30034002434f7d48c5fbbe8f
Bastien Deshayes <bastien.deshayes@kelkoogroup.com>
2019-04-12 10:11:21 +0200

    Ajout du guard pour l'authentification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ArticlesComponent} from './components/articles/articles.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {AuthGuard} from './guards/auth.guard';
 
 const routes: Routes = [
-  {path: 'articles', component: ArticlesComponent},
+  {path: 'articles', component: ArticlesComponent, canActivate: [AuthGuard]},
   {path: '', redirectTo: '/articles', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+export interface UserStatus {
+  logged: boolean;
+  oauth_url: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    return this.http.get<UserStatus>('/api/user/status').pipe(map(
+      (userStatus: UserStatus) => {
+        if (userStatus.logged) return true;
+        else return this.router.parseUrl(userStatus.oauth_url) ;
+      }
+    ));
+  }
+
+}
