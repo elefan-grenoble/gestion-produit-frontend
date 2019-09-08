ff3a29aee651bdc9600cd0122c1b61beb6324e31
Bastien Deshayes <deshayeb@gmail.com>
2019-09-08 20:31:59 +0200

    Ajout des services barcodes

--- src/app/services/barcodes.service.ts
+++ src/app/services/barcodes.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {MissingBarcode} from "../models/missing-barcode";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BarcodesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMissingBarcodes(): Observable<MissingBarcode[]> {
+    return this.http.get<MissingBarcode[]>('TODO');
+  }
+
 }
