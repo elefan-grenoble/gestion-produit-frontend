9ed81c278db2817724b251981b82ab42143d36ad
Bastien Deshayes <bastien.deshayes@kelkoogroup.com>
2019-09-18 08:58:53 +0200

    Refactoring menu

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export interface MenuItem {
+  url: string;
+  label: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -7,7 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  menuItems: MenuItem[] = [
+    {
+      url: "/supplying",
+      label: "Gérer le réapprovisonnement",
+      icon: "add_shopping_cart"
+    },
+    {
+      url: "/delivery",
+      label: "Réceptionner une livraison",
+      icon: "local_shipping"
+    },
+    {
+      url: "/missing-barcodes",
+      label: "Signaler un code barre manquant",
+      icon: "playlist_add"
+    },
+    {
+      url: "/tag-print-requests",
+      label: "Demander une impression d'étiquette",
+      icon: "print"
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
