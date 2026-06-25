d52dcbbe8d67175cd85bd9eabd718ba657fe6bcc
Raphael Odini <raphodn@users.noreply.github.com>
2026-06-25 18:40:31 +0200

    clear cache on sign in & sign out

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {UserStatus} from '../guards/auth.guard';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {ArticlesService} from './articles.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private articlesService: ArticlesService) {
   }
 
   private _userStatusSubject: BehaviorSubject<UserStatus> = new BehaviorSubject<UserStatus>(null);
@@ -18,12 +19,18 @@ export class SessionService {
   }
 
   updateUserStatus(status: UserStatus) {
+    // Clear cache when user status changes (sign in/out)
+    if (status !== this._userStatusSubject.value) {
+      this.articlesService.refreshArticles().subscribe();
+    }
     this._userStatusSubject.next(status);
   }
 
   disconnect(status: UserStatus): void {
     this.http.get<any>(status.disconnect_url).subscribe(
       (ret: any) => {
+        // Clear cache on disconnect
+        this.articlesService.refreshArticles().subscribe();
         this.updateUserStatus(null);
         window.location.href = status.logout_page_url;
       },
